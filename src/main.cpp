0b408d381ad786aed7ea3e3b55020725e6e43423
bvdka <nikita.pomies@upskills.ai>
2024-10-28 18:52:38 +0100

    create problems class

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,19 @@
 #include "../src/variables/IntVar.h"
 #include "../src/model.h"
 #include "../src/utils/graphs.h"
+#include "../src/problems/problems.h"
 // Include your class header
 using namespace std;
 
 int main()
 {
-    bool n_queens = false;
-    bool coloration = true;
+    bool n_queens = true;
+    bool coloration = false;;
     if (n_queens)
     {
 
-        Model model;
-        int n = 4;
-        // Step 1: Create variables for each row, each having domain 0 to n-1 (columns)
-        for (int i = 0; i < n; i++)
-        {
-            // model.vars.emplace_back("Q" + std::to_string(i), 0, n - 1);
-            model.add_var("Q" + std::to_string(i), 0, n - 1);
-        }
+        Model model = buildNQueens(500);
 
-        // Step 2: Add constraints: no two queens in the same column or on the same diagonal
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                // Column constraint: Q[i] != Q[j]
-                model.add_binary_cstr(&model.vars[i], &model.vars[j]);
-
-                // Diagonal constraints: Q[i] != Q[j] + (j - i)
-                model.add_binary_cstr2(&model.vars[i], &model.vars[j], j - i);    // Forward diagonal
-                model.add_binary_cstr2(&model.vars[i], &model.vars[j], -(j - i)); // Backward diagonal
-            }
-        }
         model.AC3();
         bool solved = model.solve();
         cout << model.are_constraints_entailed() << std::endl;
@@ -44,31 +25,9 @@ int main()
     }
     else
     {
-        Graph graph("../data/coloration/myciel3.col");
-        int numColors = 4;
-        // graph.printGraph();
-        Model model;
 
-        for (const auto &vertex : graph.adjacencyList)
-        {
-            int vertexID = vertex.first;
-            model.add_var("V" + std::to_string(vertexID), 0, numColors - 1);
-        }
-        for (const auto &vertex : graph.adjacencyList)
-        {
-            int u = vertex.first;
-            for (int v : vertex.second)
-            {
-                if (u < v)
-                { // Avoid duplicating constraints (since the graph is undirected)
-                    IntVar *i = model.getVarbyName("V" + std::to_string(u));
-                    IntVar *j = model.getVarbyName("V" + std::to_string(v));
-
-                    model.add_binary_cstr(i, j);
-                }
-            }
-        }
-        //model.AC3();
+        Model model = buildColorationModel("../data/coloration/myciel3.col", 4);
+        // model.AC3();
         bool solved = model.solve();
         cout << model.are_constraints_entailed() << std::endl;
 
