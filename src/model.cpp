f54b4fa04cb7e5a9584bdf835b05feb402f09ba7
bvdka <nikita.pomies@upskills.ai>
2024-10-31 21:39:44 +0100

    remove wrong for loop

--- src/model.cpp
+++ src/model.cpp
@@ -32,7 +32,7 @@ IntVar *Model::getVarbyName(const string &name)
 
 void Model::AC3()
 {
-    cout << "Starting Ac3" << endl;
+    //cout << "Starting Ac3" << endl;
     std::queue<BinaryPropagator *> ac_queue;
     std::unordered_set<BinaryPropagator *> visited; // Set to track visited propagators
     for (BinaryPropagator *propagator : propagator_queue)
@@ -55,7 +55,7 @@ void Model::AC3()
 
         if (has_been_reduced)
         {
-            // cout << "Presolving making effective reduction" << endl;
+            //cout << "Presolving making effective reduction" << endl;
             for (BinaryPropagator *propagator : propagator_queue)
             {
                 // Be careful here ?
@@ -161,72 +161,66 @@ bool Model::solve()
         return false;
     }
 
-    // Select the first unassigned variable
-    for (int i = 0; i < vars.size(); i++)
+    try
     {
-        try
-        {
-            IntVar &var = varselector.selectVariable(vars);
-            // if (&var == nullptr) return false;
+        IntVar &var = varselector.selectVariable(vars);
+        // if (&var == nullptr) return false;
 
-            if (!var.isAssigned())
+        if (!var.isAssigned())
+        {
+            std::set<int> backup_values = var.values.setvalues; // Backup the current domain
+
+            std::set<int> var_domain_backup = var.values.setvalues;
+
+            // Try each value in the domain
+            while (!backup_values.empty())
+
             {
-                std::set<int> backup_values = var.values.setvalues; // Backup the current domain
+                int min_val = valselector.selectValue(&var);
 
-                std::set<int> var_domain_backup = var.values.setvalues;
+                stats.incrementNode();
+                stats.increaseDepth();
 
-                // Try each value in the domain
-                while (!backup_values.empty())
+                backup_values.erase(min_val);
+                // Assign the variable to this value
 
+                var.instantiateTo(min_val);
+                worldPush();
+                // Propagate constraints
+                propagate_constraints();
+                // forward_checking(&var);
+                
+                //AC3();
+
+                // Check for empty domains
+                bool valid = true;
+                for (auto &v : vars)
                 {
-                    int min_val = valselector.selectValue(&var);
-
-                    stats.incrementNode();
-                    stats.increaseDepth();
-
-                    backup_values.erase(min_val);
-                    // Assign the variable to this value
-
-
-                    var.instantiateTo(min_val);
-                    worldPush();
-                    // Propagate constraints
-                    propagate_constraints();
-                    // forward_checking(&var);
-                    // AC3();
-
-                    // Check for empty domains
-                    bool valid = true;
-                    for (auto &v : vars)
+                    if (v.values.setvalues.empty())
                     {
-                        if (v.values.setvalues.empty())
-                        {
-                            valid = false;
-                            break;
-                        }
+                        valid = false;
+                        break;
                     }
-
-                    if (valid && solve())
-                    { // Recur
-                        return true;
-                    }
-                    stats.incrementBacktrack();
-                    stats.decreaseDepth();
-                    worldBack();
-                    var.values.setvalues = backup_values;
                 }
-                var.values.setvalues = var_domain_backup;
-                return false; // Return false if no value works for the current variable
+
+                if (valid && solve())
+                { // Recur
+                    return true;
+                }
+                stats.incrementBacktrack();
+                stats.decreaseDepth();
+                worldBack();
+                var.values.setvalues = backup_values;
             }
-        }
-        catch (const std::runtime_error)
-        {
-            cout << "Error detected during search" << endl;
-            return false;
+            var.values.setvalues = var_domain_backup;
+            return false; // Return false if no value works for the current variable
         }
     }
-
-    return false; // Fallback return
+    catch (const std::runtime_error)
+    {
+        cout << "Error detected during search" << endl;
+        return false;
+    }
 }
 
 bool Model::findSolution()
